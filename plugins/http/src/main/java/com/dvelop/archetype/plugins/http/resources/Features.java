7aaa6f16d0b008a5f3f8c205fe2c52c7cf40eca6
Thorsten Lanfer <thorsten.lanfer@d-velop.de>
2019-02-13 17:21:16 +0100

    use app name

--- plugins/http/src/main/java/com/dvelop/archetype/plugins/http/resources/Features.java
+++ plugins/http/src/main/java/com/dvelop/archetype/plugins/http/resources/Features.java
@@ -3,7 +3,9 @@ package com.dvelop.archetype.plugins.http.resources;
 //import com.dvelop.sdk.idp.filter.IDPRole;
 
 import com.dvelop.archetype.plugins.http.dto.FeatureDto;
+import com.dvelop.archetype.plugins.http.template.AppInfo;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -26,6 +28,9 @@ public class Features {
         }
     }
 
+    @Inject
+    AppInfo appInfo;
+
     @GET
     @Produces({MediaType.APPLICATION_JSON, "application/hal+json"})
     public Response getFeatures() {
@@ -37,7 +42,7 @@ public class Features {
                         "Manage vacation requests",
                         "Apply for vacation and approve the vacation requests of your employees",
                         "Your vacation requests and the requests of your employees",
-                        "/vacationprocess4j"+List.PATH,
+                        "/" + appInfo.getName() + "/"+List.PATH,
                         "#adff2f",
                         "dv-search"
                 )
